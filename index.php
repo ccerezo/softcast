5f953bee8740ae78fd0a62a79c8ede013b45c618
ccerezo <ccerezo90@gmail.com>
2015-06-16 13:33:11 -0500

    Primer version en Github: contabilidad: 85%, Bancos: 70%, Cuentas por Pagar: 10%

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
     <head>
 	<title>Sistema SOFTCAST</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     	<meta name="viewport" content="width=device-width, initial-scale=1" />
     	
     	<link rel="stylesheet" href="css/bootstrap.css" />
